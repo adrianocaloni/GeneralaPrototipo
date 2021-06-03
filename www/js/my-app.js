728f5e9c4a2061eaeeeea451834f5a10f6bacab1
josecerda <cerda.jose.manuel@gmail.com>
2021-06-03 01:43:12 -0300

    Arreglo de index, funciones, acomodo de UI

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -36,10 +36,10 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 
 /*      VARIABLES GLOBALES      */
-var nombreJugador1 = '';
-var nombreJugador2 = '';
-var nombreJugador3 = '';
-var nombreJugador4 = '';
+var nombreJugador1 = 'Jugador 1';
+var nombreJugador2 = 'Jugador 2';
+var nombreJugador3 = 'Jugador 3';
+var nombreJugador4 = 'Jugador 4';
 var ganador = '';
 var puntajeJ1 = 0;
 var puntajeJ2 = 0;
@@ -53,17 +53,18 @@ $$(document).on('deviceready', function() {
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
-    $$('#jugador1').val('');
-    $$('#jugador2').val('');
-    $$('#jugador3').val('');
-    $$('#jugador4').val('');
+    $$('#jugador1').val(nombreJugador1);
+    $$('#jugador2').val(nombreJugador2);
+    $$('#jugador3').val(nombreJugador3);
+    $$('#jugador4').val(nombreJugador4);
 })
 
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
-    nombreJugador1 = '';
-    nombreJugador2 = '';
-    nombreJugador3 = '';
-    nombreJugador4 = '';
+
+    //nombreJugador1 = '';
+    //nombreJugador2 = '';
+    //nombreJugador3 = '';
+    //nombreJugador4 = '';
 
     //fnMostrarMasJugadores(); (Para revisar, no borrar)
 
@@ -72,6 +73,12 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
       nombreJugador2 = $$('#jugador2').val();
       nombreJugador3 = $$('#jugador3').val();
       nombreJugador4 = $$('#jugador4').val();
+      ganador = '';
+      puntajeJ1 = 0;
+      puntajeJ2 = 0;
+      puntajeJ3 = 0;
+      puntajeJ4 = 0;
+
       if ($$('#cantJugadores').val() == 2 && nombreJugador1 != '' && nombreJugador2 != '') {
         app.views.main.router.navigate("/anotador/");
       }
@@ -81,7 +88,7 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
       if ($$('#cantJugadores').val() == 4 && nombreJugador1 != '' && nombreJugador2 != '' && nombreJugador3 != '' && nombreJugador4 != '') {
         app.views.main.router.navigate("/anotador/");
       }
-      
+
     })
 
     $$('#finProvisorio').on('click', function(){
@@ -101,7 +108,7 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
     var id = '';
 
     /*      EVENTOS     */
-    
+
     /* 1A */
     $$('.btnIngresaValor').on('click', function(){
       id = $$(this).attr('id');
@@ -114,11 +121,11 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
     })
 
     /* 2A */
-    $$('.eligeAnotacion').on('click', function(){ 
+    $$('.eligeAnotacion').on('click', function(){
       var valorElegido = $$(this).val();
       console.log(valorElegido);
 
-      fnGuardaValor(valorElegido);  
+      fnGuardaValor(valorElegido);
     })
 
     $$('.btnFinaliza').on('click', function(){
@@ -126,7 +133,7 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
         console.log('opcion'+opcion);
       if (opcion == 'Limpiar') {
         fnLimpiar();
-      } 
+      }
       if (opcion == 'Terminar') {
         fnTerminar();
       }
@@ -137,9 +144,9 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
       app.popup.close('.my-popup-nro', true);
       app.popup.close('.my-popup-finalizar',true);
     })
-  
+
     /*      FUNCIONES     */
-    
+
     /* (Para revisar!)
     function fnMostrarMasJugadores() {
       if ($$('#cantJugadores').val() == 3) {
@@ -155,8 +162,8 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
         $$('#puntuacionFinal3').removeClass('hidden').addClass('visible');
         $$('#puntuacionFinal4').removeClass('hidden').addClass('visible');
       }
-    }*/ 
-    
+    }*/
+
 
     function fnMuestraMultiplos() {
       $$('#op1Dado').val(idSoloNro*1);
@@ -176,9 +183,11 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
             nro = 20;
           } else if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           }
           break;
-  
+
         case 8:
           if (valor == 'Servida') {
             nro = 35;
@@ -186,9 +195,11 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
             nro = 30;
           } else if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           }
           break;
-  
+
         case 9:
           if (valor == 'Servida') {
             nro = 45;
@@ -196,9 +207,11 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
             nro = 40;
           } else if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           }
           break;
-  
+
         case 10:
           if (valor == 'Servida') {
             nro = 55;
@@ -206,9 +219,11 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
             nro = 50;
           } else if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           }
           break;
-  
+
         case 11:
           if (valor == 'Servida') {
             nro = 105;
@@ -216,15 +231,19 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
             nro = 100;
           } else if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           }
           break;
-  
+
         default:
           if (valor == 'Tachar') {
             nro = '---';
+          } else if (valor == 'Borrar') {
+            nro = '';
           } else {
             nro = valor;
-          } 
+          }
         }
         $$('#'+id).val(nro);
         app.popup.close('.my-popup-fig', true);
@@ -232,23 +251,23 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
         fnSumaTotales();
       }
 
-    function fnSumaTotales() {  
+    function fnSumaTotales() {
       var puntaje = 0;
       var finDeJuego = true;
       for (var j=1; j<3; j++) {
         puntaje = 0;
         for (var i=1; i<12; i++) {
-          if ($$('#j'+j+'-'+i).val() == '---') {} 
+          if ($$('#j'+j+'-'+i).val() == '---') {}
           else  if ($$('#j'+j+'-'+i).val() == "") {
             finDeJuego = false;
           }
-          else {  
+          else {
             var p = $$('#j'+j+'-'+i).val();
             puntaje += parseInt(p);
           }
         }
-        $$('#totalJ'+j).val(puntaje);  
-      } 
+        $$('#totalJ'+j).val(puntaje);
+      }
       fnControlFinDeJuego(finDeJuego);
     }
 
@@ -264,15 +283,15 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
           console.log(ganador);
         }
         if (puntajeJ2>puntajeJ1 && puntajeJ2>puntajeJ3 && puntajeJ2>puntajeJ4) {
-          ganador = nombreJugador1;
+          ganador = nombreJugador2;
           console.log(ganador);
         }
         if (puntajeJ3>puntajeJ1 && puntajeJ3>puntajeJ2 && puntajeJ3>puntajeJ4) {
-          ganador = nombreJugador1;
+          ganador = nombreJugador3;
           console.log(ganador);
         }
         if (puntajeJ4>puntajeJ1 && puntajeJ4>puntajeJ2 && puntajeJ4>puntajeJ3) {
-          ganador = nombreJugador1;
+          ganador = nombreJugador4;
           console.log(ganador);
         }
         app.views.main.router.navigate("/anotador/");
@@ -286,12 +305,12 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
       for (var j=1; j<3; j++) {
         for (var i=1; i<12; i++) {
           $$('#j'+j+'-'+i).val("");
-        }   
+        }
       }
       $$('#totalJ1').val("0");
-      $$('#totalJ2').val("0"); 
-      $$('#totalJ3').val("0"); 
-      $$('#totalJ4').val("0"); 
+      $$('#totalJ2').val("0");
+      $$('#totalJ3').val("0");
+      $$('#totalJ4').val("0");
       app.popup.close('.my-popup-finalizar', true);
     }
 
@@ -308,7 +327,7 @@ $$(document).on('page:init', '.page[data-name="fin"]', function (e) {
   $$("#puntuacionFinal2").html(nombreJugador2 + ": " + puntajeJ2);
   $$("#puntuacionFinal3").html(nombreJugador2 + ": " + puntajeJ3);
   $$("#puntuacionFinal4").html(nombreJugador2 + ": " + puntajeJ4);
-  
+
   $$('#opCancelar').on('click', function(){
     app.views.main.router.navigate("/index/");
   })
